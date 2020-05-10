0df003814ebff77f356851dd34cbc03777da61c0
Amit Bhavikati <amitmbhavikatti@gmail.com>
2020-05-10 17:29:38 +0530

    Initial commit: simple-fd-calc-app

--- App.js
+++ App.js
@@ -1,36 +1,66 @@
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
+import { Platform, StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [rateOfInterest, setRateOfInterest] = React.useState(0);
+  const [depositAmount, setDepositAmount] = React.useState(0);
+  const [frequency, setFrequency] = React.useState(0);
+  const maturityAmount = 4 * frequency * depositAmount * (1 + rateOfInterest / 25)
+
+  const isValidNumber = (val) => {
+    const numericInput = parseFloat(val);
+    return !Number.isNaN(numericInput) && typeof numericInput === 'number'
+  }
+
+  const setFieldValue = (value, setHook) => {
+    isValidNumber(value) ? setHook(value.toString()) : ""
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
+    <View style={{ width: "80%", height: "80%", marginTop: 60 , marginLeft: 10, marginRight: 10, marginBottom: 30}}>
+      <View>
+        <Text>Deposit Amount: </Text>
+        <TextInput
+          label='Deposit Amount'
+          value={depositAmount}
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          maxLength={20}
+          keyboardType='numeric'
+          onChangeText={depositAmountString => {
+            setFieldValue(depositAmountString, setDepositAmount)
+          }}
+        />
+
+        <Text>Rate Of Interest:</Text>
+        <TextInput
+          label='Rate Of Interest'
+          value={rateOfInterest}
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          keyboardType='numeric'
+          maxLength={10}
+          onChangeText={rate => {
+            setFieldValue(rate, setRateOfInterest)
+
+          }}
+        />
+
+        <Text>Compounded Interest Frequency:</Text>
+        <TextInput
+          label='Compounded Interest Frequency'
+          value={frequency}
+          maxLength={10}
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          keyboardType='numeric'
+          onChangeText={frequencyString => {
+            setFieldValue(frequencyString, setFrequency)
+          }}
+        />
+      </View>
+
+      <View>
+        <Text>Maturity Amount: {isValidNumber(maturityAmount) ? maturityAmount.toString() : ""}</Text>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
